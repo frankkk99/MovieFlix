c7ade2ff9ea544657264f7212bffbb60d6bdfc33
frankkk99 <am2.production29@gmail.com>
2025-09-29 08:02:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   // CONFIG
-  const API_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9....";
+  const API_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMTFhOTNkZDhhZjI4ZWM1ZThjNTA1MjczODNkZmY2YSIsIm5iZiI6MTc1ODUyMzY5OC4xNzQsInN1YiI6IjY4ZDBmMTMyY2E3YzE3OTM0YWFjODRmZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jkk1DSO189wR1ByDvR4O0uAygqoLsQjgsHeD5uBwOFU";
   const BASE_URL = "https://api.themoviedb.org/3";
   const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
 
